1edd13e3fddf5f60c938d361967243315d944517
marcin10lw <marcin10lw@wp.pl>
2023-05-21 20:35:21 +0200

    styled Navigation tablet view

--- src/common/Navigation/index.tsx
+++ src/common/Navigation/index.tsx
@@ -2,6 +2,7 @@ import styles from "./index.module.scss";
 import logo from "../../assets/logo.svg";
 import avatar from "../../assets/image-avatar.jpg";
 import { ReactComponent as Moon } from "../../assets/icon-moon.svg";
+import { ReactComponent as Sun } from "../../assets/icon-sun.svg";
 
 const Navigation = () => {
   return (
@@ -15,7 +16,7 @@ const Navigation = () => {
         />
       </div>
 
-      <div>
+      <div className={styles.wrapper}>
         <div className={styles.themeWrapper}>
           <input type="checkbox" id="check" />
           <label htmlFor="check">
