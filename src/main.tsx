c125ab7f8a4ac845228130d1d9600cfc500820d9
marcin10lw <marcin10lw@wp.pl>
2023-08-15 22:28:58 +0200

    configured queryClient

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./App";
 import { ThemeProvider } from "./context/ThemeContext";
 import "./sass/globals/globalStyles.scss";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <HashRouter>
       <ThemeProvider>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </ThemeProvider>
     </HashRouter>
   </React.StrictMode>
