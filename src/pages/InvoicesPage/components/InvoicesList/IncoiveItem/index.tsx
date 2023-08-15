3cf6a1d3de00b4916353535a64c78bd4249df31d
marcin10lw <marcin10lw@wp.pl>
2023-08-15 22:29:35 +0200

    created InvoiceItem

---
+++ src/pages/InvoicesPage/components/InvoicesList/IncoiveItem/index.tsx
@@ -0,0 +1,45 @@
+import { Invoice } from "types";
+import { ReactComponent as Arrow } from "assets/icon-arrow-right.svg";
+import styles from "./index.module.scss";
+
+const InvoiceItem = ({
+  id,
+  paymentDue,
+  clientName,
+  total,
+  status,
+}: Invoice) => {
+  const formattedTotal = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "GBP",
+  }).format(total);
+
+  return (
+    <li className={styles.invoiceWrapper}>
+      <article className={styles.invoice}>
+        <div className={styles.invoice__leftContent}>
+          <div className={styles.leftContentWrapper}>
+            <h2 className={styles.invoice__id}>
+              <span>#</span>
+              {id}
+            </h2>
+            <p className={styles.invoice__paymentDue}>Due {paymentDue}</p>
+          </div>
+          <p className={styles.invoice__clientName}>{clientName}</p>
+        </div>
+        <div className={styles.invoice__rightContent}>
+          <div className={styles.rightContentWrapper}>
+            <p className={styles.invoice__total}>{formattedTotal}</p>
+            <div className={styles.invoice__status}>
+              <p>{status}</p>
+            </div>
+          </div>
+
+          <Arrow className={styles.invoice__arrow} />
+        </div>
+      </article>
+    </li>
+  );
+};
+
+export default InvoiceItem;
---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export type Invoice = {
+  id: string;
+  paymentDue: string;
+  clientName: string;
+  total: number;
+  status: string;
+};
