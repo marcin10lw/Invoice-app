4f18d1fc3db49c73d6676a5ae07b7a4a10fb2a07
marcin10lw <marcin10lw@wp.pl>
2023-08-28 14:12:52 +0200

    styled desktop bill from

--- src/common/Form/BillFrom/index.tsx
+++ src/common/Form/BillFrom/index.tsx
@@ -1,25 +1,37 @@
+import styles from "./index.module.scss";
+
 const BillFrom = () => {
   return (
     <div>
-      <fieldset>
-        <legend>Bill From</legend>
+      <fieldset className={styles.fieldset}>
+        <legend className={`legend`}>Bill From</legend>
 
         <div>
-          <div>
-            <label htmlFor="street">Street Address</label>
-            <input id="street" name="street" />
+          <div className="inputGroup">
+            <label htmlFor="street" className="label">
+              Street Address
+            </label>
+            <input id="street" name="street" className="input" />
           </div>
-          <div>
-            <label htmlFor="city">City</label>
-            <input id="city" name="city" />
-          </div>
-          <div>
-            <label htmlFor="postCode">Post Code</label>
-            <input id="postCode" name="postCode" />
-          </div>
-          <div>
-            <label htmlFor="coutry">Country</label>
-            <input id="country" name="country" />
+          <div className={styles.inputsWrapper}>
+            <div className="inputGroup">
+              <label htmlFor="city" className="label">
+                City
+              </label>
+              <input id="city" name="city" className="input" />
+            </div>
+            <div className="inputGroup">
+              <label htmlFor="postCode" className="label">
+                Post Code
+              </label>
+              <input id="postCode" name="postCode" className="input" />
+            </div>
+            <div className="inputGroup">
+              <label htmlFor="coutry" className="label">
+                Country
+              </label>
+              <input id="country" name="country" className="input" />
+            </div>
           </div>
         </div>
       </fieldset>
