98bbd84544551f74b7c67d328a161b8f09c61b1d
marcin10lw <marcin10lw@wp.pl>
2023-09-04 11:24:41 +0200

    created datepicker logic

---
+++ src/utils/calender.ts
@@ -0,0 +1,39 @@
+import dayjs from "dayjs";
+
+export const generateDate = (
+  month = dayjs().month(),
+  year = dayjs().year()
+) => {
+  const firstDateOfMonth = dayjs().year(year).month(month).startOf("month");
+  const lastDateOfMonth = dayjs().year(year).month(month).endOf("month");
+
+  const arrayOfDate = [];
+
+  for (let i = firstDateOfMonth.date(); i <= lastDateOfMonth.date(); i++) {
+    arrayOfDate.push({
+      date: firstDateOfMonth.date(i),
+      currentMonth: true,
+      today:
+        firstDateOfMonth.date(i).toDate().toDateString() ===
+        dayjs().toDate().toDateString(),
+    });
+  }
+
+  const remaining = 35 - arrayOfDate.length;
+
+  for (
+    let i = lastDateOfMonth.date() + 1;
+    i <= lastDateOfMonth.date() + remaining;
+    i++
+  ) {
+    arrayOfDate.push({
+      date: lastDateOfMonth.date(i),
+      currentMonth: false,
+      // today:
+      //   firstDateOfMonth.date(i).toDate().toDateString() ===
+      //   dayjs().toDate().toDateString(),
+    });
+  }
+
+  return arrayOfDate;
+};
